4abcf70b3b9e7d33ad5c862a0f8d7190c9474c42
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-12-25 21:33:31 +0900

    feat: Implement room list crawler

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import asyncio
 
 from scripts.building import fetch_building_list
+from scripts.room import fetch_room_list
 
 if __name__ == "__main__":
     buildings = asyncio.run(fetch_building_list())
+    rooms = asyncio.run(fetch_room_list(buildings))
---
+++ src/scripts/room.py
@@ -0,0 +1,33 @@
+import asyncio
+
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+async def fetch_room_list(building_posts: list[dict]) -> list[dict]:
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for building_post in building_posts:
+            tasks.append(asyncio.create_task(
+                fetch_room_page(session, building_post.get("title"), building_post.get("link"))))
+        values = await asyncio.gather(*tasks)
+    room_list = []
+    for value in values:
+        room_list.extend(value)
+    return room_list
+
+
+async def fetch_room_page(session, name, url) -> list[dict]:
+    room_data = []
+    async with session.get(url) as response:
+        raw_data = []
+        response_text = await response.text()
+        soup = BeautifulSoup(response_text, "html.parser")
+        room_list = soup.select("div > div > div > table > tbody > tr > td")
+        for room in room_list:
+            data = room.text.strip()
+            if data and data != "\u200b":
+                raw_data.append(data)
+    for index in range(0, len(raw_data) - 1, 2):
+        room_data.append({"building": name, "number": raw_data[index], "name": raw_data[index + 1]})
+    return room_data
